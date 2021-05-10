ddd84a8649b132258a2cb95b063aa969405ca019
dank-tagg <mianzhihu@gmail.com>
2021-05-10 17:14:14 +0200

    edited ipc server route

--- main/bot.py
+++ main/bot.py
@@ -36,7 +36,7 @@ class GrootBot(commands.Bot):
         self.cached_users = {}
         self.cached_disabled = {}
         self.tips_on_cache = set()
-        self.ipc = ipc.Server(self, host="us3.derp.host", secret_key="GrootBotAdmin")
+        self.ipc = ipc.Server(self, secret_key="GrootBotAdmin")
 
     async def after_db(self):
         """Runs after the db is connected"""
