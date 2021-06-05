307947c5cc34943e5525e0ffabda01e54a926d67
dank-tagg <mianzhihu@gmail.com>
2021-06-05 15:34:16 +0200

    Deleted cache log

--- main/utils/cache.py
+++ main/utils/cache.py
@@ -3,8 +3,7 @@ from datetime import datetime
 class CacheManager(dict):
 
     def __init__(self):
-        # Initialize the dict with the items listed
-        self.log = ""
+        pass
 
     @property
     def length(self):
@@ -18,13 +17,10 @@ class CacheManager(dict):
 
 
     def __setitem__(self, key, value):
-        self.log += self.do_log(f"SET `{key}` with value `{value}`")
         return super().__setitem__(key, value)
     
     def __getitem__(self, key):
-        self.log += self.do_log(f"GET `{key}` with value `{super().__getitem__(key)}`")
         return super().__getitem__(key)
 
     def get(self, key, default=None):
-        self.log += self.do_log(f"GET `{key}` with value `{super().__getitem__(key)}`")
         return super().get(key, default)
