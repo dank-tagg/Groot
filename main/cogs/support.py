711d0d7e5da63daee6848cf8096f358c3eb98501
dank-tagg <79766992+dank-tagg@users.noreply.github.com>
2021-06-24 11:03:28 +0200

    Update support.py

--- main/cogs/support.py
+++ main/cogs/support.py
@@ -65,6 +65,18 @@ class Support(commands.Cog):
                 raise commands.BadArgument(
                     "`60s` are over. I ended your report session, since you didn't answer fast enough. Next time please be quicker."
                 )
+    
+    
+    @commands.command(name="suggest")
+    async def _suggest_feature(self, ctx, *, suggestion):
+        channel = self.bot.get_channel(857544734338449448)
+        msg = await channel.send(f"{ctx.author}: {suggestion}")
+        await msg.add_reaction("👍")
+        await msg.add_reaction("👎")
+    
+    @commands.command(name="support")
+    async def _support_server_invite(self, ctx):
+        await ctx.send("https://discord.gg/nUUJPgemFE")
 
 
 def setup(bot):
